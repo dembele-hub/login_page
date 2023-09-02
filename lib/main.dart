06bae5de215eab10da26006561a208f00332359c
dembele_hub <chrisdembele5@gmail.com>
2023-09-02 12:58:59 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const LoginScreen());
+}
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  LoginScreen1State createState() => LoginScreen1State();
+}
+
+class LoginScreen1State extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final emailField = TextField(
+      style: const TextStyle(fontSize: 20.0),
+      decoration: InputDecoration(
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final passwordField = TextField(
+      obscureText: true,
+      style: const TextStyle(fontSize: 20.0),
+      decoration: InputDecoration(
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
+    );
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: const Color(0xff01A0C7),
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: const Text("Login",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    return SingleChildScrollView(
+      child: Center(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height / 1.5,
+          child: Padding(
+            padding: const EdgeInsets.all(36.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  height: 100.0,
+                  child: Image.asset(
+                    "image/image_logo.png.jpg",
+                    fit: BoxFit.contain,
+                  ),
+                ),
+                const SizedBox(height: 48.0),
+                emailField,
+                const SizedBox(height: 24.0),
+                passwordField,
+                const SizedBox(
+                  height: 36.0,
+                ),
+                loginButton,
+                const SizedBox(
+                  height: 16.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
