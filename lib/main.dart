9169c066458e689f06711899e7b2a872bc21e6de
dembele_hub <chrisdembele5@gmail.com>
2023-09-05 15:30:00 +0100

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,88 +4,141 @@ void main() => runApp(const MaterialApp(
   home:  LoginScreen(),
 ));
 
+
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+  const LoginScreen({super.key});
 
   @override
-  LoginScreenState createState() => LoginScreenState();
+  State<LoginScreen> createState() => _LoginScreen();
 }
 
-class LoginScreenState extends State<LoginScreen> {
+class _LLoginScreen2State extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     final emailField = TextField(
       style: const TextStyle(fontSize: 20.0),
       decoration: InputDecoration(
-        contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        hintText: "Email",
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Email",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(16.0))),
     );
 
     final passwordField = TextField(
       obscureText: true,
       style: const TextStyle(fontSize: 20.0),
       decoration: InputDecoration(
-        contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        hintText: "Password",
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
+          contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          hintText: "Password",
+          border:
+          OutlineInputBorder(borderRadius: BorderRadius.circular(16.0))),
     );
 
-    final loginButton = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: const Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {},
-        child: const Text(
-          "Login",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20.0,
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-
-    return SingleChildScrollView(
-      child: Center(
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height / 1.5,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  height: 100.0,
-                  child: Image.asset(
-                    "image/image_logo.png.jpg", // Correct the image asset path
-                    fit: BoxFit.contain,
+    const pageColor = Colors.pink;
+    return Container(
+      child: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints viewportConstraints) {
+            return SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: viewportConstraints.maxHeight,
+                ),
+                child: Container(
+                  color: pageColor,
+                  child: Container(
+                    child: IntrinsicHeight(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            height: MediaQuery
+                                .of(context)
+                                .size
+                                .height * 0.35,
+                            width: MediaQuery
+                                .of(context)
+                                .size
+                                .width,
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment
+                                      .stretch,
+                                  mainAxisSize: MainAxisSize.max,
+                                  children: <Widget>[
+                                    CircleAvatar(
+                                      radius: 40,
+                                      backgroundColor: pageColor.shade100,
+                                      child: Icon(
+                                        Icons.person,
+                                        size: 48,
+                                        color: pageColor.shade800,
+                                      ),
+                                    ),
+                                    const Padding(
+                                      padding: EdgeInsets.all(16.0),
+                                      child: Text(
+                                        "Login",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                            fontStyle: FontStyle.italic),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: pageColor.shade50,
+                                    borderRadius: const BorderRadius.only(
+                                        topLeft: Radius.circular(56.0),
+                                        topRight: Radius.circular(56.0))),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(16.0),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.max,
+                                    crossAxisAlignment: CrossAxisAlignment
+                                        .stretch,
+                                    mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                    children: <Widget>[
+                                      const SizedBox(height: 48.0),
+                                      emailField,
+                                      const SizedBox(height: 24.0),
+                                      passwordField,
+                                      const SizedBox(
+                                        height: 36.0,
+                                      ),
+                                      FloatingActionButton(
+                                        onPressed: () {},
+                                        backgroundColor: pageColor,
+                                        child: const Icon(
+                                          Icons.arrow_forward,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        height: 16.0,
+                                      ),
+                                    ],
+                                  ),
+                                )),
+                          )
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                const SizedBox(height: 48.0),
-                emailField,
-                const SizedBox(height: 24.0),
-                passwordField,
-                const SizedBox(
-                  height: 36.0,
-                ),
-                loginButton,
-                const SizedBox(
-                  height: 16.0,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
+              ),
+            );
+          }),
     );
   }
 }
